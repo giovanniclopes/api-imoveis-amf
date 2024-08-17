070e7045304edede2bf462d2c651ad09cb5654e8
Giovanni Lopes <giovanniclopes@gmail.com>
2024-08-17 05:26:56 -0300

    fix font import

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BackToTopButton } from "./components/BackToTopButton";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
+import "./assets/fonts/urbane/Urbane_Light.otf";
+import "./assets/fonts/urbane/Urbane_Bold.otf";
 
 function App() {
   return (
